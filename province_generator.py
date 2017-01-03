c66198afac113d9bbf97eb9c1edfd8a9583e0ea5
CuriouslyCurious <thealmightywarrior@gmail.com>
2017-01-03 11:30:18 +0100

    Modified province_generator.py to read from provinces.bmp

--- province_generator.py
+++ province_generator.py
@@ -1,20 +1,39 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 
+from PIL import Image
+
 import os
+import codecs
 
 prov_dir = os.getcwd()+"\\shatterednippon\\history\\provinces\\"
-local_dir = os.getcwd()+"\\shatterednippon\localisation\\"
+local_dir = os.getcwd()+"\\shatterednippon\\localisation\\"
+map_dir = os.getcwd()+"\\shatterednippon\\map\\"
 
 local_file = open(local_dir + "prov_names_l_english.yml", "w")
 local_file.write("l_english:\n")
 
+definitions = open(map_dir + "definition.csv", "w")
+definitions.write("province;red;green;blue;x;x\n")
+
+im = Image.open(map_dir+"provinces.bmp")
+colors = []
+
+image_size = im.size
+
+for x in range(0, image_size[0]):
+	for y in range(0, image_size[1]):
+		cord = (x,y)
+		pixel = im.getpixel(cord)
+		if pixel not in colors and not (pixel[0:3] == (255,255,255) or pixel[0:3] == (0,0,0)): # excluding pure white and black
+			colors.append(pixel) 
+
 land_tiles = 451
 sea_tiles = 91
-provinces = land_tiles + sea_tiles
+provinces = len(colors)
 
-
-for x in range(0, provinces):
+x = 0
+for color in colors:
 	letter = (x%26) + 65
 	out = "%d - " % (x+1)
 	if x > 25:
@@ -45,7 +64,12 @@ capital = "{1}"
 
 discovered_by = chinese""".format(out, out.split(" - ")[1]))
 	local_file.write(' PROV{0}:0 "{1}"\n'.format(x+1, out.split(" - ")[1]))
+	
+	definitions.write("%d;%s;%s;%s;;%s\n" % (x+1, color[0], color[1], color[2], out.split(" - ")[1]))
+	
 	f.close()
+
+	x += 1
+	
 local_file.close()
-
-
+definitions.close()
