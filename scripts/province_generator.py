d4ec375c2aa9c31ad512d54c83ce3025787de3de
CuriouslyCurious <thealmightywarrior@gmail.com>
2017-02-15 19:57:30 +0100

    Ran provice_generator.py

--- scripts/province_generator.py
+++ scripts/province_generator.py
@@ -43,26 +43,31 @@ for y in range (image_size[1]):
 """
 colors = im.getcolors(maxcolors=10000) # Arbitrary maxcolors number
 for color in colors:
-    if color[1][2] > 0:
+    color = color[1]
+    if color[2] > 0:
         sea_tiles += 1
     else:
         land_tiles += 1
 
 print("land: ", land_tiles)
 print("sea: ", sea_tiles)
-sys.exit()
 provinces = len(colors)
 
 x = 0
 for color in colors:
-	letter = (x%26) + 65
-	out = "%d - " % (x+1)
-	if x > 25:
-		out += chr((x//26) + 64)
-	out += chr(letter)
-	if (x < land_tiles):
-		f = open(prov_dir + out + ".txt", "w")
-		f.write	(
+    color = color[1]
+    if color[2] == 0:
+        out = "%d - PROV%d" % (x+1, x+1)
+        """
+            letter = (x%26) + 65
+            out = "%d - " % (x+1)
+            if x > 25:
+                out += chr((x//26) + 64)
+            out += chr(letter)
+        """
+        if (x < land_tiles):
+            f = open(prov_dir + out + ".txt", "w")
+            f.write	(
 """# {0}
 
 owner = JAP
@@ -84,15 +89,15 @@ trade_goods = silk
 capital = "{1}"
 
 discovered_by = chinese""".format(out, out.split(" - ")[1]))
-	local_file.write(' PROV{0}:0 "{1}"\n'.format(x+1, out.split(" - ")[1]))
-	
-	definitions.write("{0};{1};{2};{3};;{4}\n".format(x+1, color[0], color[1], color[2], out.split(" - ")[1]))
-	
-	#definitions.write("{0};{1};{2};{3};;{4}\n".format(x+1, color[1][0], color[1][1], color[1][2], out.split(" - ")[1]))
-	
-	f.close()
+        local_file.write(' PROV{0}:0 "{1}"\n'.format(x+1, out.split(" - ")[1]))
+        
+        definitions.write("{0};{1};{2};{3};;{4}\n".format(x+1, color[0], color[1], color[2], out.split(" - ")[1]))
+        
+        #definitions.write("{0};{1};{2};{3};;{4}\n".format(x+1, color[1][0], color[1][1], color[1][2], out.split(" - ")[1]))
+        
+        f.close()
 
-	x += 1
+        x += 1
 
 local_file.close()
 definitions.close()
